4f1aa869c410233f071e5669f6cb135930e0d27e
davedan <dherraiz@outlook.com>
2019-04-06 09:59:08 +0200

    Adding Portainer

--- hassio_install_rpi3.sh
+++ hassio_install_rpi3.sh
@@ -16,3 +16,18 @@ sudo rm hassio_install
 sudo wget https://raw.githubusercontent.com/home-assistant/hassio-build/master/install/hassio_install
 sudo chmod +x hassio_install
 sudo ./hassio_install -d /config/homeassistant -m raspberrypi3
+
+echo "Portainer Installation - Cleaning current container ..."
+sudo docker stop "Portainer"
+sudo docker rm "Portainer"
+
+echo "Portainer Installation - Cleaning images ..."
+sudo docker images
+sudo docker rmi portainer/portainer
+
+echo "Portainer Installation - Downloading image ..."
+sudo docker pull portainer/portainer
+sudo docker images
+
+echo "Portainer Installation - Creating the new container ..."
+sudo docker run -d --name="Portainer" --restart="always" -d -p 1024:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
